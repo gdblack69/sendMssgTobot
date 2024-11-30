910c4e9a0b520557c337519c1a7b5930ac538891
gdblack69 <geetasaaroj@gmail.com>
2024-11-30 16:30:42 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,10 +8,8 @@ def home():
     return "Bot is running..."
 
 def run():
-    # Run the Flask app in production mode (set threaded=True for multiple requests handling)
-    app.run(host="0.0.0.0", port=8080, threaded=True)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    # Run the Flask app in a separate thread to keep it alive
     t = threading.Thread(target=run)
     t.start()
