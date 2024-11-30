328c3ee473b5ee1267b69f6a88efbc509c1fc892
gdblack69 <geetasaaroj@gmail.com>
2024-11-30 15:40:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# API credentials for source chat
+SOURCE_API_ID = 26697231
+SOURCE_API_HASH = '35f2769c773534c6ebf24c9d0731703a'
+SOURCE_CHAT_ID = -4564401074
+
+# API credentials for destination account
+DESTINATION_API_ID = 14135677
+DESTINATION_API_HASH = 'edbecdc187df07fddb10bcff89964a8e'
+DESTINATION_BOT_USERNAME = '@gpt3_unlim_chatbot'
+
+# Paths for session files
+SOURCE_SESSION_FILE = "new10_source_session.session"
+DESTINATION_SESSION_FILE = "new10_destination_session.session"
