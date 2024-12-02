5ca0a920215313d2a85535baf6c5ce3db9c84dae
gdblack69 <geetasaaroj@gmail.com>
2024-12-02 18:50:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'GreyMatters'
-
-
-if __name__ == "__main__":
-    app.run()
+    return "Hello, World!"
